c436d57c77bfa29a2cc8914ad0f02b44b06fd93a
Bhupendra Dodiya <bdbhupendra123@gmail.com>
2023-03-01 21:21:06 +0530

    api testing

---
+++ thunder/app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+    return render(request,'index.html')
+
+def showall(request):
+    data = requests.get('http://tanveerpp.pythonanywhere.com/product/')
+    data = data.json()
+    return render(request,'showdata.html',{'data':data})
+
+def showone(request):
+    if request.method =="POST":
+        id = request.POST['id']
+        data = requests.get('http://tanveerpp.pythonanywhere.com/product/'+id)
+        data = data.json()
+        return render(request,'showdata.html',{'data':[data]})
+    
+def add(request):
+    if request.method =="POST":
+        name = request.POST['name']
+        price = request.POST['price']
+        category = request.POST['category']
+        company = request.POST['company']
+        details = {'name':name,'price':price,'cat':category,'cmp':company}
+        requests.post('http://tanveerpp.pythonanywhere.com/product/',details)
+        data = requests.get('http://tanveerpp.pythonanywhere.com/product/')
+        data = data.json()
+        return render(request,'showdata.html',{'data':data})
+    
+def delete(request):
+    id = request.GET['id']
+    requests.delete('http://tanveerpp.pythonanywhere.com/product/'+id)
+    data=requests.get('http://tanveerpp.pythonanywhere.com/product/')
+    data=data.json()
+    return render(request,'showdata.html',{'data':data})
+
+
+
+        
